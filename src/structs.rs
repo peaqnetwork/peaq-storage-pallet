d5489f9f7088532fe4e1b0923925ec1c30131636
lavish0000 <lavick100@gmail.com>
2022-09-26 19:41:35 +0530

    feat: add createItem, updateItem, getItem extrinsics

---
+++ src/structs.rs
@@ -0,0 +1,13 @@
+use codec::{Decode, Encode};
+use scale_info::TypeInfo;
+use sp_core::RuntimeDebug;
+use sp_std::vec::Vec;
+
+/// Attributes of a User.
+#[derive(
+	PartialEq, Eq, PartialOrd, Ord, Clone, Encode, Decode, Default, RuntimeDebug, TypeInfo,
+)]
+pub struct UserAttribute {
+	pub profile: Vec<u8>,
+	pub sessions: Vec<u8>,
+}
