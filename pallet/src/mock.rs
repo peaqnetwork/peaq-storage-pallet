64c1d8092651293ed7f4fb5734b5f7b644f7f116
jaypan <pan@peaq.io>
2024-02-20 16:18:41 +0100

    Move to 1.1.0

--- pallet/src/mock.rs
+++ pallet/src/mock.rs
@@ -2,23 +2,19 @@ use crate as peaq_storage;
 use frame_support::parameter_types;
 use frame_system as system;
 use sp_core::{sr25519, Pair, H256};
+use sp_runtime::BuildStorage;
 use sp_runtime::{
-    testing::Header,
     traits::{BlakeTwo256, IdentityLookup},
 };
 
-type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 type Block = frame_system::mocking::MockBlock<Test>;
 
 // Configure a mock runtime to test the pallet.
 frame_support::construct_runtime!(
-    pub enum Test where
-        Block = Block,
-        NodeBlock = Block,
-        UncheckedExtrinsic = UncheckedExtrinsic,
+    pub enum Test
     {
-        System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
-        PeaqStorage: peaq_storage::{Pallet, Call, Storage, Event<T>},
+        System: frame_system,
+        PeaqStorage: peaq_storage,
     }
 );
 
@@ -33,16 +29,15 @@ impl system::Config for Test {
     type BaseCallFilter = frame_support::traits::Everything;
     type BlockWeights = ();
     type BlockLength = ();
+    type Nonce = u64;
+    type Block = Block;
     type DbWeight = ();
     type RuntimeOrigin = RuntimeOrigin;
     type RuntimeCall = RuntimeCall;
-    type Index = u64;
-    type BlockNumber = u64;
     type Hash = H256;
     type Hashing = BlakeTwo256;
     type AccountId = sr25519::Public;
     type Lookup = IdentityLookup<Self::AccountId>;
-    type Header = Header;
     type RuntimeEvent = RuntimeEvent;
     type BlockHashCount = BlockHashCount;
     type Version = ();
@@ -67,8 +62,8 @@ impl peaq_storage::Config for Test {
 
 // Build genesis storage according to the mock runtime.
 pub fn new_test_ext() -> sp_io::TestExternalities {
-    system::GenesisConfig::default()
-        .build_storage::<Test>()
+    system::GenesisConfig::<Test>::default()
+        .build_storage()
         .unwrap()
         .into()
 }
