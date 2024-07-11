aa7d9fb722bc1ac2c7b1b819c95e848488a817b5
Jay Pan <pan@peaq.io>
2024-07-11 10:34:38 -0700

    Upgrade to 1.3.0

--- pallet/src/mock.rs
+++ pallet/src/mock.rs
@@ -76,6 +76,7 @@ impl pallet_balances::Config for Test {
     type MaxHolds = ();
     type MaxFreezes = ();
     type RuntimeHoldReason = ();
+    type RuntimeFreezeReason = ();
 }
 parameter_types! {
     pub const MinimumPeriod: u64 = 5;
