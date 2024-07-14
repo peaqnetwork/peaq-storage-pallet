621028b2221ecbe0cf194999ef0685f02877f99e
Jay Pan <pan@peaq.io>
2024-07-14 14:26:49 -0700

    Update rpc

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -2,10 +2,14 @@ use std::convert::From;
 use std::sync::Arc;
 
 use jsonrpsee::{
-    core::{async_trait, Error as JsonRpseeError, RpcResult},
+    core::{async_trait, RpcResult},
     proc_macros::rpc,
-    types::error::{CallError, ErrorObject},
+    types::{
+        error::{INTERNAL_ERROR_CODE, INTERNAL_ERROR_MSG},
+        ErrorObjectOwned,
+    },
 };
+
 use parity_scale_codec::Codec;
 use parity_scale_codec::{Decode, Encode};
 pub use peaq_pallet_storage_runtime_api::PeaqStorageApi as PeaqStorageRuntimeApi;
@@ -82,12 +86,15 @@ where
         let api = self.client.runtime_api();
         api.read(at, did_account, item_type.to_vec())
             .map(|o| o.map(StorageRpcResult::from))
-            .map_err(|e| {
-                JsonRpseeError::Call(CallError::Custom(ErrorObject::owned(
-                    Error::RuntimeError.into(),
-                    "Unable to get value.",
-                    Some(format!("{:?}", e)),
-                )))
-            })
+            .map_err(|err| internal_err(err.to_string()))
     }
+}
+
+// This bit cribbed from frontier.
+pub fn internal_err<T: ToString>(message: T) -> ErrorObjectOwned {
+    ErrorObjectOwned::owned(
+        INTERNAL_ERROR_CODE,
+        INTERNAL_ERROR_MSG,
+        Some(message.to_string()),
+    )
 }
